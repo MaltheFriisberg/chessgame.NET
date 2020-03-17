817863e546abc717fafd297a8c64db097eaee50d
Malthe Friisberg <malthe.friisberg@azets.com>
2020-03-17 09:01:54 +0100

    add Extension method and test

---
+++ AiEngine.Tests/Extensions.Tests/IntegerExtensionTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AiEngine.Tests.Extensions.Tests
+{
+    [TestClass]
+    public class IntegerExtensionTest
+    {
+        [TestMethod]
+        public void TestIntegerExtension() {
+            Assert.IsFalse(true);
+        }
+        
+    }
+}
---
+++ AiEngine/Extensions/IntegerExtension.cs
@@ -0,0 +1,13 @@
+namespace Extensions {
+
+    public static class IntegerExtension {
+        /// <summary>
+        /// Extension method for pretty printing a 64 bit integer as a chess board
+        /// </summary>
+        /// <param name="board"></param>
+        public static void PrintBoard(this long board) {
+            
+        }
+    }
+
+}
